ebed9ca531ea440e8070616413d73f31c49e30cd
flat35hd99 <flat35hd99@gmail.com>
2021-12-20 17:34:53 +0900

    Fix

--- get_unchanged_files.sh
+++ get_unchanged_files.sh
@@ -3,16 +3,14 @@
 before=$1;
 after=$2;
 
-echo $before;
-echo $after;
+echo "Unchanged files:"
 
 git ls-files | while read -r line;
 do
-    st=$(git diff --name-status $before $after $line);
-    echo $st;
-    echo "Unchanged files:";
-    if [ -n $st ]; then
-        echo $st;
-        touch --date=@0 $st;
+    st=$(git diff --name-status "$after" "$before" "$line");
+    if [ -n "$st" ]; then
+        unchanged_file=$(echo $st | awk '{print $2}')
+        echo $unchanged_file
+        touch --date=@0 $unchanged_file
     fi;
 done
