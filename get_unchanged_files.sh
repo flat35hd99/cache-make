35bd2f4f01804434a0d1cf08ff9775fefc6b9baa
flat35hd99 <flat35hd99@gmail.com>
2021-12-20 18:27:32 +0900

    Fix workflow scripts

--- get_unchanged_files.sh
+++ get_unchanged_files.sh
@@ -3,14 +3,16 @@
 before=$1;
 after=$2;
 
-echo "Unchanged files:"
-
 git ls-files | while read -r line;
 do
     st=$(git diff --name-status "$after" "$before" "$line");
     if [ -n "$st" ]; then
-        unchanged_file=$(echo $st | awk '{print $2}')
-        echo $unchanged_file
+        changed_file=$(echo $st | awk '{print $2}')
+        echo "Changed file: ${changed_file}"
+        # touch --date=@0 $unchanged_file
+    else
+        unchanged_file=$line
+        echo "Unchanged file: ${unchanged_file}"
         touch --date=@0 $unchanged_file
     fi;
 done
